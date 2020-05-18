7ae7087d945d311273dce8f7e4fa6ad39ffb536c
lcy785739757 <785739757@qq.com>
2020-05-18 13:09:40 +0800

    项目坏了，重新构建

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Main from '../components/Main'
 import Create from '../components/management/Create'
+import Release from "../components/blink/Release";
+import Browse from "../components/blink/Browse";
+import Wel from "../components/wel";
+import wel from "../components/wel";
 // const Login = () => import('../components/Login')
 // import Main from "../components/Main";
 
@@ -23,13 +27,29 @@ const routes = [
   {
     path:'/main',
     name:'main',
-    component:Main
+    component:Main,
+    redirect: '/wel',
+    children:[{path:'/wel',name:'wel',component:Wel},
+      { path:'/Release', name:'release', component:Release},
+      { path:'/Create', name:'create', component:Create},
+      { path:'/Browse', name:'browse', component:Browse},
+    ]
   },
-  {
-    path:'/management/create',
-    name:'create',
-    component:Create
-  },
+  // {
+  //   path:'/create',
+  //   name:'create',
+  //   component:Create
+  // },
+  // {
+  //   path:'/blink/Release',
+  //   name:'release',
+  //   component:Release
+  // },
+  // {
+  //   path:'/blink/Browse',
+  //   name:'browse',
+  //   component:Browse
+  // }
 ]
 
 const router = new VueRouter({
